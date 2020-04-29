271556ed000b7fb59ec56caf325e179d193032b2
Niklas Loeser <niklas@4loeser.net>
2020-04-29 14:53:04 +0200

    some random comm mit

--- MatrixMultiplikation_Crystal/matrixmultiplication.cr
+++ MatrixMultiplikation_Crystal/matrixmultiplication.cr
@@ -3,24 +3,24 @@ require "benchmark"
 
 module MatrixMultiplication
 	
-  	@@r = Random.new
+  @@r = Random.new
 
-	def self.makeMatrix(size) : Array(Array(Float64))
-  		arr = Array(Array(Float64)).new
+	def self.makeMatrix(size) : Array(Array(Float32))
+  		arr = Array(Array(Float32)).new
     	size.times do |x|
-   	    	row = Array(Float64).new
+   	    	row = Array(Float32).new
       		size.times do |y|
-    	    	row << @@r.next_float
+    	    	row << @@r.next_float.to_f32
       		end
       		arr << row
     	end
   		arr
  	end
   
-  def self.multiply(m1, m2)
+  def self.multiply(m1, m2, arr)
     m2Width = m2[0].size
-    arr = Array(Array(Float64)).new(m1.size, Array(Float64).new)
-    row = Array(Float64).new(m2Width,0)
+    row = Array(Float32).new(m2Width,0)
+    total = uninitialized Float32
     m1.size.times do |x| # zeile in m1
       m2Width.times do |y|# spalte in m2
         total = 0.0
@@ -42,7 +42,8 @@ THREADS = 24
 
 m1 = MatrixMultiplication.makeMatrix(SIZE)
 m2 = MatrixMultiplication.makeMatrix(SIZE)
+arr = Array(Array(Float32)).new(m1.size, Array(Float32).new)
 
 puts Benchmark.realtime {
-  m3 = MatrixMultiplication.multiply(m1, m2)
+  m3 = MatrixMultiplication.multiply(m1, m2, arr)
 }
